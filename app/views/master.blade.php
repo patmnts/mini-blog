b01ec5684053f70f8c27d0a68713a37ae0414ca6
Samuel UC <sam_edreiuc@live.com>
2013-12-11 11:05:39 +0100

    V.app.views.master: Construction de template master

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,39 +1,53 @@
 
 
-<!--DOCTYPE html-->
-	<head>
-		<meta charset="utf-8">
-		<title>@yield('title') - Mini Blog - </title>
-		
-        {{HTML::style('template/css/style.css')}}
-        {{HTML::style('template/css/bootstrap.css')}}
-	</head>
-    <body>
-    	<header>
-    		<div class="row">
-    			<div class="col-md-8">Mini Blog</div>
-    			<div class="col-md-4">Logo</div>
-    		</div>
-    	</header>
-    	
-    	<div class="container col-md-8">
-            @yield('content')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title') - Mini Blog - </title>
+            {{HTML::style('template/css/style.css')}}
+            {{HTML::style('template/css/bootstrap.css')}}
+    </head>
+    <body class="body">
+        <header class="mainHeader">
+            <div class="row">
+                <div class="headerTitle"><h1>Mini Blog</h1></div>
+            </div>
+        </header>
+
+        <div class="mainContent">
+            <div class="content">
+                <article class="topContent">
+                    <header>
+                        <h2>Bienvenue User</h2>
+                    </header>
+                        <footer>
+                            <p class="post-info">ici l'information de l'article</p>
+                        </footer>
+                    <content>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel semper dui, vel suscipit tortor. Sed at ligula orci. Quisque vehicula pellentesque mi, condimentum pellentesque diam dignissim at. Morbi accumsan bibendum nulla. Maecenas elementum ligula quis fringilla imperdiet. Duis dolor quam, gravida non neque venenatis, laoreet ultrices massa. In laoreet pretium mi sit amet tincidunt. Nulla ac dictum eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam sit amet eros quis mauris ornare elementum nec suscipit est. Sed a leo quam. Proin in ipsum convallis, lobortis justo eget, consequat elit. Etiam vestibulum in leo a congue.</p>
+                    </content>
+                </article>
+            </div>
         </div>
+        <aside class="top-sidebar">
+            <article>
+                <h2>Menu</h2>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Porfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </article>
+        </aside>
+
+        <aside class="bottom-sidebar">
+            <article>
+                <h2>nouvelles</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel semper dui, vel suscipit tortor. Sed at ligula orci. Quisque vehicula pellentesque mi, condimentum pellentesque diam dignissim at. </p>
+            </article>
+        </aside>
         
-
-        <div class="sidebar col-md-4">
-        	@yield('side')
-        </div>
-
-        <div id="menu"> 
-        	<div class="col-md-4">
-				Aqui va el menu
-        	</div>
-        </div>
-
-        
-
-        <footer>This is the footer</footer>
-    
+        <footer class="mainFooter"><p>Copyright &copy; Mini-blog </p></footer>
     </body>
 </html>
