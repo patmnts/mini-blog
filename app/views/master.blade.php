5596735e341e45989ccb04feb6e8ba9e0d75ce42
Samuel UC <sam_edreiuc@live.com>
2013-12-12 09:38:51 +0100

    V.app.views.master: test de @yield contenu

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -19,17 +19,8 @@
 
         <div class="mainContent">
             <div class="content">
-                <article class="topContent">
-                    <header>
-                        <h2>Bienvenue User</h2>
-                    </header>
-                        <footer>
-                            <p class="post-info">ici l'information de l'article</p>
-                        </footer>
-                    <content>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel semper dui, vel suscipit tortor. Sed at ligula orci. Quisque vehicula pellentesque mi, condimentum pellentesque diam dignissim at. Morbi accumsan bibendum nulla. Maecenas elementum ligula quis fringilla imperdiet. Duis dolor quam, gravida non neque venenatis, laoreet ultrices massa. In laoreet pretium mi sit amet tincidunt. Nulla ac dictum eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam sit amet eros quis mauris ornare elementum nec suscipit est. Sed a leo quam. Proin in ipsum convallis, lobortis justo eget, consequat elit. Etiam vestibulum in leo a congue.</p>
-                    </content>
-                </article>
+                @yield('contenu') 
+                
             </div>
         </div>
         <aside class="top-sidebar">
