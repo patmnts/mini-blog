b6458c23ce44ad34c546b8192a95a968e2f7c2c1
Samuel UC <sam_edreiuc@live.com>
2013-12-13 14:55:51 +0100

    adaptation de la collection master

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -5,10 +5,7 @@
     <head>
         <meta charset="utf-8">
         <title>@yield('title') - Mini Blog - </title>
-            {{HTML::style('template/css/style.css')}}
-            {{HTML::style('bower_components/bootstrap/dist/css/bootstrap.css')}}
-            {{HTML::style('bower_components/font-awesome/css/font-awesome.css')}}
-            {{HTML::script('bower_components/bootstrap/dist/js/bootstrap.js')}}
+        {{ Basset::show('master.css') }}
     </head>
     <body class="body">
         <header class="mainHeader">
@@ -20,7 +17,6 @@
         <div class="mainContent">
             <div class="content">
                 @yield('contenu') 
-                
             </div>
         </div>
         <aside class="top-sidebar">
@@ -28,7 +24,7 @@
                 <ul class="nav nav-list">
                     <li class="nav-header"><h4>MENU</h4></li>
                         <li class="active"><a href="#"><i class="icon-home"></i> Accueil</a></li>
-                        <li><a href="#"><i class="icon-bookmark"></i> Portofiol</a></li>
+                        <li><a href="create"><i class="icon-bookmark"></i> Portfolio</a></li>
                         <li><a href="#"><i class="icon-bookmark"></i> Contact</a></li>
                         <li><a href="#"><i class="icon-dashboard"></i> Dashboard</a></li>
                         <li><a href="#"><i class="icon-bookmark"></i> souscrire</a></li>
