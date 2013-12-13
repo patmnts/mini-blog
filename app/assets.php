75974db586d02da91eb2baa7b91c5db3672f89f2
Samuel UC <sam_edreiuc@live.com>
2013-12-13 14:52:09 +0100

    A.assets: ajout des liens

--- app/assets.php
+++ app/assets.php
@@ -6,8 +6,10 @@
 
 Basset::collection('master', function($collection) {
     // CSS
-    $collection->stylesheet('template/css/bootstrap.css');
     $collection->stylesheet('template/css/style.css');
+    $collection->stylesheet('bower_components/bootstrap/dist/css/bootstrap.css');
+    $collection->stylesheet('bower_components/font-awesome/css/font-awesome.css');
+    $collection->stylesheet('bower_components/bootstrap/dist/js/bootstrap.js');
 })
 ->rawOnEnvironment('local')
 ->apply('CssMin')
