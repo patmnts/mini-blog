cbd6a3ff13ea6d51a7228c8eaac5c8bc9b0030e8
Samuel UC <sam_edreiuc@live.com>
2014-01-13 17:09:29 +0100

    app.config.database: config de basse de données

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'mini-blog',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
