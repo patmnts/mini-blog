5bd02619d4a978038758e439f17828fb6bcd1a25
Samuel UC <sam_edreiuc@live.com>
2014-01-15 17:14:44 +0100

    V.article.show: ajout des boutons ajout article et modidier

--- app/views/article/show.blade.php
+++ app/views/article/show.blade.php
@@ -17,6 +17,11 @@
 			<h5>by {{$element->author}}</h5>
 			<h5>{{$element->date}}</h5>
 		</content>
+		<p><a href="create?{{$element->idarticle}}" class="btn btn-primary">modifier l'article</a></p>
 	</article>
 	@endforeach
+	{{ $articles->links() }}
+	<p>
+    	<a href="create" class="btn btn-primary">ajout un article</a>
+  	</p>
 @stop
