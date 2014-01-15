a0d1a6fb792511a75d8eb0f0552561aabe10f8dc
Samuel UC <sam_edreiuc@live.com>
2014-01-15 15:06:30 +0100

    V.users: style topcontent

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -9,7 +9,7 @@
 			<h2>Bienvenue User</h2>
 		</header>
 				<footer>
-					<p class="post-info">ici l'information de l'article</p>
+					<h3><p class="post-info">ici l'information de l'article</p></h3>
 				</footer>
 		<content>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel semper dui, vel suscipit tortor. Sed at ligula orci. Quisque vehicula pellentesque mi, condimentum pellentesque diam dignissim at. Morbi accumsan bibendum nulla. Maecenas elementum ligula quis fringilla imperdiet. Duis dolor quam, gravida non neque venenatis, laoreet ultrices massa. In laoreet pretium mi sit amet tincidunt. Nulla ac dictum eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam sit amet eros quis mauris ornare elementum nec suscipit est. Sed a leo quam. Proin in ipsum convallis, lobortis justo eget, consequat elit. Etiam vestibulum in leo a congue.</p>
